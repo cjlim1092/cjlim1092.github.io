976671b75bbef0422a067babe249d635b823888b
cjlim1092 <32859465+cjlim1092@users.noreply.github.com>
2020-08-04 21:19:02 -0700

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -11,12 +11,13 @@ let myFlashcards = [
       question: "What is 3+3",
       answer: 4
       }
-  for (let i =0; i < myFlashcards.length; i++) 
+  for (let i =0; i < myFlashcards.length; i++) {
     console.log(myFlashcards[i].question + ' ' + myFlashcards[i].answer);
     document.querySelector('html').onclick = function() {
     alert(myFlashcards[i].question);
     document.querySelector('html').onclick = function() {
     alert(myFlashcards[i].answer);
+    }
 }
 
 ];
