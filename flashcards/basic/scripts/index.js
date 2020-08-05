3311b8047b85d0d9dd5b94f4ab305ce1c1bc2c2e
cjlim1092 <32859465+cjlim1092@users.noreply.github.com>
2020-08-04 20:47:16 -0700

    Create index.js

---
+++ flashcards/basic/scripts/index.js
@@ -0,0 +1,22 @@
+let myFlashcards = [
+  {
+    question: "What is 1+1",
+    answer: 2
+    },
+  {
+    question: "What is 2+2",
+    answer: 4
+    },
+  {
+      question: "What is 3+3",
+      answer: 4
+      }
+  for (let i =0; i < myFlashcards.length; i++) 
+    console.log(myFlashcards[i].question + ' ' + myFlashcards[i].answer);
+    document.querySelector('html').onclick = function() {
+    alert(myFlashcards[i].question);
+    document.querySelector('html').onclick = function() {
+    alert(myFlashcards[i].answer);
+}
+
+];
