a12edc71f5129ed2fe94777b88ada21bebd5939d
cjlim1092 <32859465+cjlim1092@users.noreply.github.com>
2020-08-07 20:15:00 -0700

    Update index.js

--- flashcards/v4/scripts/index.js
+++ flashcards/v4/scripts/index.js
@@ -6,21 +6,25 @@
 let myFlashcards = [
   {
     question: "What is 1+1",
-    answer: 2
+    answer: 2,
+    image: 'images/onePlusOne.jpg'
     },
   {
     question: "What is 2+2",
-    answer: 4
+    answer: 4,
+    image: 'images/twoplustwo.jpg'
     },
   {
       question: "What is 3+3",
-      answer: 6
+      answer: 6,
+      image: 'images/threePlusThree.jpg'
   }
 ];
 //declare variables
 var i= 0; 
 var q = 0; //counter to make sure loop in answer click works
-
+console.log ('D i is now '+ i);
+console.log('D q is '+ q);
 
 //get question
 
@@ -30,28 +34,31 @@ function getQuestion() {
   q += 1;
   console.log('Q q is '+ q);
 }
-//default question
-function defaultText()
-        {
-          document.getElementById("displayHere").innerHTML = "Flashcard";
-        }
+
+//get images
+function getImages() {
+  //add picture
+  var img = document.getElementById("displayImages").setAttribute("src", myFlashcards[i].image);
+
+  console.log('getImages I is '+ i );
+  console.log(myFlashcards[i].image);
+}
           
 //get answer
 function getAnswer() {
+ console.log ('A i is now '+ i);
+ console.log('A q is '+ q);
   if (q > i){
   document.getElementById("displayAnswerHere").innerHTML = myFlashcards[i].answer;}
   else{
   document.getElementById("displayAnswerHere").innerHTML = "Please look at the question first";
   }
 }
-//default answer
-function defaultAnswerText()
-        {
-            document.getElementById("displayAnswerHere").innerHTML = "Flashcard Answer";
-        }
+
           
 //loop to get next question
 function getNext() {
+  // document.getElementById("displayHere").innerHTML = "  ";
   //iterate to next card
   if (i===myFlashcards.length){
     i = 0; 
